d3c4109bd56bddde67f82e30602fa24eaa94cff5
JoseAlberto2022 <118962309+JoseAlberto2022@users.noreply.github.com>
2023-05-11 11:56:40 +0200

    Add files via upload

---
+++ ExamenBlancoYNegro3T.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+import math 
+contador = 0
+for root, dirs, files in os.walk("./carpetadelexamen/"):
+    for filename in files:
+        print(filename)
+        if contador > 0:
+            imagen = Image.open("./carpetadelexamen/"+filename)
+            pixeles = imagen.load()
+            altura = imagen.size[1]
+            anchura = imagen.size[0]
+            for x in range(0,anchura):
+                for y in range(0,altura):
+                    rojo = pixeles[x,y][0]
+                    verde = pixeles[x,y][1]
+                    azul = pixeles[x,y][2]
+                    color = math.floor((rojo+verde+azul)/3)
+                    rojo = rojo
+                    verde = rojo
+                    azul = rojo
+                    pixeles[x,y] = (rojo,verde,azul)              
+            imagen.save("./carpetadelexamen/"+filename)
+        contador += 1
+
